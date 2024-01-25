ee989ebf351d57e3dc8bd92168b01428b04b140c
Thupden Bhutia <47317869+thupden@users.noreply.github.com>
2024-01-24 23:14:19 -0800

    Add files via upload

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,62 @@
+import Input from "./Input.jsx";
+import Button from "./Button.jsx";
+import { useRef } from "react";
+import Modal from "./Modal.jsx";
+
+export default function NewProject({ onAdd, onCancel }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+  const modal = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      //show the error modal
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops... looks like you forget to enter a value
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <Button onClick={onCancel}>Cancel</Button>
+          </li>
+          <li>
+            <Button onClick={handleSave}>Save</Button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
+      </div>
+    </>
+  );
+}
